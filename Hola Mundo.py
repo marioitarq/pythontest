249e9d806dc063536a59b244549ee3182b29dd15
mario_itarq <mario.cabrero@gmail.com>
2022-11-05 21:24:06 +0100

    Add files via upload

---
+++ Hola Mundo.py
@@ -0,0 +1,17 @@
+### Hola Mundo ###
+
+# Nuestro hola mundo en Python
+print("Hola Python")
+print('Hola Python')
+# Nuestro hola mundo en Python
+print("Hola Python")
+print('Hola Python')
+# Nuestro hola mundo en Python
+print("Hola Python")
+print('Hola Python')
+# Nuestro hola mundo en Python
+print("Hola Python")
+print('Hola Python')
+# Nuestro hola mundo en Python
+print("Hola Python")
+print('Hola Python')
